b7e8389452c6a283b3e9e0ffadd592da8be24bf9
sakeel jawfer <sakeeljawfer@gmail.com>
2021-01-20 10:07:21 +0530

    first commit

---
+++ BookStoresWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+
+        [HttpGet]
+        public IEnumerable<Publisher> Get()
+        {
+            //CRUD
+            using (var _context = new BookStoresDBContext())
+            {
+                //Publisher publisher = new Publisher();
+                // publisher.PublisherName = "Egmont Book";
+
+                //_context.Publishers.Add(publisher);
+
+                // _context.SaveChanges();
+
+                Publisher publisher = _context.Publishers.FirstOrDefault();
+                //publisher.PublisherName = "Egmont Books";
+
+                _context.Publishers.Remove(publisher);
+
+
+                _context.SaveChanges();
+
+                return _context.Publishers.ToList();
+
+
+            }
+            //return new List<Publisher>();
+
+        }
+    }
+}
---
+++ BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DELL;Initial Catalog=BookStoresDB;Integrated Security=True");
+        }
+    }
+}
